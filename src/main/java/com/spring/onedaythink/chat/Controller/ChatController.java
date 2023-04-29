362e4895b697a854e92f5d4bc641aeb409dcf234
DESKTOP-HC4CT58\initbyran <kmr4766@naver.com>
2023-04-30 00:47:24 +0900

    2023-04-29 숙제 중

---
+++ src/main/java/com/spring/onedaythink/chat/Controller/ChatController.java
@@ -0,0 +1,41 @@
+package com.spring.onedaythink.chat.Controller;
+
+import com.spring.onedaythink.chat.service.ChatService;
+import com.spring.onedaythink.chat.vo.ChatMessage;
+import com.spring.onedaythink.chat.vo.ChatRoom;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.method.P;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping(value = "chat")
+public class ChatController {
+
+    private Logger log = LogManager.getLogger("case3");
+
+    @Autowired
+    private ChatService chatService;
+
+    // 마지막 메시지 조회
+    @GetMapping(value="/{chatRoomNo}")
+    public ResponseEntity getLastMessage(@PathVariable int chatRoomNo){
+        ChatRoom chatRoom = ChatRoom.builder().chatRoomNo(chatRoomNo).build();
+        ChatMessage lastMessage = chatService.getLastMessage(chatRoom);
+        log.debug("getLastMessage");
+        return ResponseEntity.ok(lastMessage);
+    }
+
+    // 채팅 종료
+
+
+
+
+}
