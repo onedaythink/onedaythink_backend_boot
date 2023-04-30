c9d1935f85eb70c510f0186b316db009086bcff6
DESKTOP-HC4CT58\initbyran <kmr4766@naver.com>
2023-04-30 11:51:07 +0900

    2023-04-30 논제 CRUD 수정빼고 완료, 타인과의 대화 마지막 메시지 조회 구현 완료, 페르소나 하루봇 조회, 선택 후 채팅 시작 구현 관련 테이블 내용 변경 method 구현 완료

--- src/main/java/com/spring/onedaythink/chat/Controller/ChatController.java
+++ src/main/java/com/spring/onedaythink/chat/Controller/ChatController.java
@@ -8,11 +8,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.method.P;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
