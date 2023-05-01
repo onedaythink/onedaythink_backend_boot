40948edfecf4c491bf20a6d1a8acdb6db9ac6f52
sjh <zwielicht2@gmail.com>
2023-05-01 09:13:02 +0900

    :Fix - main logger remove

--- src/main/java/com/spring/onedaythink/chat/Controller/ChatController.java
+++ src/main/java/com/spring/onedaythink/chat/Controller/ChatController.java
@@ -8,11 +8,14 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.method.P;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @RestController
-@RequestMapping(value = "chat")
+@RequestMapping(value = "api/v1/chat")
 public class ChatController {
 
     private Logger log = LogManager.getLogger("case3");
@@ -29,9 +32,30 @@ public class ChatController {
         return ResponseEntity.ok(lastMessage);
     }
 
-    // 채팅 종료
+    // 해당 사용자가 가지고 있는 채팅방 목록 조회
+    @GetMapping(value = "/rooms/{userNp}")
+    public ResponseEntity<Object> getCharRooms(@PathVariable int userNo){
 
+        log.debug("# All Chat Rooms");
 
+        return ResponseEntity.ok(null);
+    }
 
+    //채팅방 개설
+    @PostMapping(value = "/room")
+    public ResponseEntity<Object> addChatRoom(@RequestBody ChatRoom chatRoom){
+        log.debug("# Create Chat Room , name: " + chatRoom.toString());
+
+        return ResponseEntity.ok(null);
+    }
+
+    //채팅방 요청 승인/거절 관리
+//    @PostMapping("/room/process")
+//    public ResponseEntity<Object> (String roomId, Model model){
+//
+//        log.info("# get Chat Room, roomID : " + roomId);
+//
+//        return ResponseEntity.ok(null);
+//    }
 
 }
